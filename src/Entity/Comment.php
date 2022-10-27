73a774ea7a8ba5649f8844bdc75938bd72a9f0b8
Hyeonmin Seon <hmsun.es@esgroup.net>
2022-10-27 18:00:39 +0900

    chapter 18 진행중

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -54,6 +54,11 @@ class Comment
      */
     private $photoFilename;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $state;
+
     public function __toString(): string
     {
         return (string) $this->getEmail();
@@ -144,5 +149,17 @@ class Comment
     public function setCreatedAtValue()
     {
         $this->createdAt = new \DateTimeImmutable();
+    }
+
+    public function getState(): ?string
+    {
+        return $this->state;
+    }
+
+    public function setState(string $state): self
+    {
+        $this->state = $state;
+
+        return $this;
     }
 }
