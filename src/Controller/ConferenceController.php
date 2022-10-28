3f8a96c7b02069e272954d7607cf1cc8d740b5b1
Hyeonmin Seon <hmsun.es@esgroup.net>
2022-10-28 17:29:17 +0900

    캐시 처리

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -35,6 +35,19 @@ class ConferenceController extends AbstractController
     {
         return $this->render('conference/index.html.twig', [
             'conferences' => $conferenceRepository->findAll(),
+        ])->setSharedMaxAge(3600);
+    }
+
+    /**
+     * 
+     * @Route("/conference_header",name="conference_header")
+     * @param ConferenceRepository $conferenceRepository 
+     * @return Response 
+     */
+    public function conferenceHeader(ConferenceRepository $conferenceRepository): Response
+    {
+        return $this->render('conference/header.html.twig', [
+            'conferences' => $conferenceRepository->findAll()
         ]);
     }
 
